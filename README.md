58eaa1ca08c4d4374a5bdeed946f681a343521db
babaochou2420 <babaochou2420@gmail.com>
2024-10-19 16:54:46 +0800

    just complete skeleton of part 2

--- README.md
+++ README.md
@@ -1,3 +1,32 @@
 # DM2024-Lab1-Master
 
 ISA5810 Lab 1 Notebook
+
+## 環境安裝
+
+1. 環境安裝
+ - Python 3.9.6 https://www.python.org/downloads/release/python-396/
+2. 複製專案
+    ``` 
+    git clone https://github.com/CavalryHill/DM2024-Lab1-Master.git
+    ```
+3. 套件安裝
+ - 虛擬環境
+  ```
+  py -3.9 -m venv .venv
+  ```
+  ```
+  .\.venv\Scripts\activate
+  ```
+- 資源安裝
+  ```
+  pip install -U -r .\requirements.txt
+  ```
+  ```
+  python ./requirements.py
+  ```
+- 執行資源
+  ```
+  jupyter notebook
+  ```
+
---
+++ requirements.py
@@ -0,0 +1,21 @@
+import nltk
+
+# [nltk_data] Downloading package punkt to
+# [nltk_data]     C:\Users\user\AppData\Roaming\nltk_data...
+#
+# Look Up Error
+#
+# Searched in:
+# - 'C:\\Users\\user/nltk_data'
+# - 'c:\\Users\\user\\Documents\\school_work\\113-1-data_mining\\lab_1\\.venv\\nltk_data'
+# - 'c:\\Users\\user\\Documents\\school_work\\113-1-data_mining\\lab_1\\.venv\\share\\nltk_data'
+# - 'c:\\Users\\user\\Documents\\school_work\\113-1-data_mining\\lab_1\\.venv\\lib\\nltk_data'
+# - 'C:\\Users\\user\\AppData\\Roaming\\nltk_data'
+# - 'C:\\nltk_data'
+# - 'D:\\nltk_data'
+# - 'E:\\nltk_data'
+# - 'C:\\Users\\user\\AppData\\Roaming\\nltk_data'
+#
+# nltk.download("punkt", force=True)
+
+nltk.download()
